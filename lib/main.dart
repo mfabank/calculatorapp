3f1e006be47b6019d476245b582d9b8180fdfde2
MFA <53713274+mfabank@users.noreply.github.com>
2020-09-30 01:36:55 +0300

    start

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator App"),
+        centerTitle: true,
+      ),
+
+    );
+  }
+}
